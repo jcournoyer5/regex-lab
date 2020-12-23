077b37ce450c24400420f009538c5e4c1eb29df4
Jacqueline Cournoyer <jcournoyer5@gmail.com>
2020-12-23 17:01:03 +0000

    Done.

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,6 +1,6 @@
 def starts_with_a_vowel?(word)
     
-   "The rain in Spain lies mainly in the plain".match(/\b[aeiouAEIOU]/g)
+   "The rain in Spain lies mainly in the plain".match(/\b[aeiouAEIOU]/)
 
 end
 
